490a792a9f914a3c40c197c2c1c441f995040512
Michael Knox <knoxy5467@gmail.com>
2024-03-26 21:07:07 -0400

    created task table entity and added it to the mod file

---
+++ server/src/database/task.rs
@@ -0,0 +1,15 @@
+use chrono::NaiveDateTime;
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "task")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub title: String,
+    pub completed: bool,
+    pub last_edited: chrono::NaiveDateTime,
+}
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+impl ActiveModelBehavior for ActiveModel {}
