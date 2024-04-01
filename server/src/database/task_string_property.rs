ff4e1c253426d1c23c7bc6d1b4bc48309169fb10
Michael Knox <knoxy5467@gmail.com>
2024-04-01 16:50:43 -0400

    corrected table name

--- server/src/database/task_string_property.rs
+++ server/src/database/task_string_property.rs
@@ -1,6 +1,6 @@
 use sea_orm::entity::prelude::*;
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
-#[sea_orm(table_name = "task_bool_property")]
+#[sea_orm(table_name = "task_string_property")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub task_id: i32,
