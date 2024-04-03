08ac6ff4e8d96d18d95c8cee3046d4eca234ecf1
Michael Knox <knoxy5467@gmail.com>
2024-04-03 18:20:09 -0400

    add settings json to git ignore add num property basic tests

--- server/src/database/task_num_property.rs
+++ server/src/database/task_num_property.rs
@@ -13,17 +13,11 @@ pub struct Model {
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
     #[sea_orm(
-        belongs_to = "super::task_property::Entity",
+        belongs_to = "super::task::Entity",
         from = "Column::TaskId",
-        to = "super::task_property::Column::TaskId"
+        to = "super::task::Column::TaskId"
     )]
     Task,
-    #[sea_orm(
-        belongs_to = "super::task_property::Entity",
-        from = "Column::Name",
-        to = "super::task_property::Column::Name"
-    )]
-    Name,
 }
 impl Related<super::task_property::Entity> for Entity {
     fn to() -> RelationDef {
@@ -31,3 +25,25 @@ impl Related<super::task_property::Entity> for Entity {
     }
 }
 impl ActiveModelBehavior for ActiveModel {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_copy_clone_debug() {
+        let original = Relation::Task;
+        let copy = original;
+        assert_eq!(original, copy);
+        let clone = original.clone();
+        assert_eq!(original, clone);
+        format!("{:?}", original);
+    }
+
+    #[test]
+    fn test_enum_iter() {
+        let mut iter = Relation::iter();
+        assert_eq!(iter.next(), Some(Relation::Task));
+        assert_eq!(iter.next(), None);
+    }
+}
