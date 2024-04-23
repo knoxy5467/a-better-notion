860a1ab85c25fc86630f38e4ba5f8fe4b2c33b11
knoxy5467 <knoxy5467@gmail.com>
2024-04-23 18:10:03 -0400

    fixed calling th modify_view correctly

--- client/src/ui/task_create_popup.rs
+++ client/src/ui/task_create_popup.rs
@@ -57,7 +57,7 @@ impl TaskCreatePopup {
                     name: self.name.clone(),
                     ..Default::default()
                 });
-                state.modify_view(state.get_default_view(), |v| {
+                state.modify_view(state.get_default_view().db_id, |v| {
                     v.tasks.as_mut().unwrap().push(task_id)
                 });
                 self.should_close = true;
